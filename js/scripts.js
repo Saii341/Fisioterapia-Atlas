aa72a1c85cd5974b577955cf0f51084df8631b47
Saii341 <patlanerick341@gmail.com>
2025-11-27 22:11:43 -0600

    lalala

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,19 @@ window.addEventListener('DOMContentLoaded', event => {
     const listHoursArray = document.body.querySelectorAll('.list-hours li');
     listHoursArray[new Date().getDay()].classList.add(('today'));
 })
+window.addEventListener('DOMContentLoaded', function() {
+    const navbar = document.getElementById('mainNav');
+    const body = document.body;
+    
+    window.addEventListener('scroll', function() {
+        const scrollPosition = window.scrollY;
+        
+        if (scrollPosition > 100) { // Cambia después de 100px de scroll
+            navbar.classList.add('scrolled');
+            body.classList.add('has-fixed-navbar');
+        } else {
+            navbar.classList.remove('scrolled');
+            body.classList.remove('has-fixed-navbar');
+        }
+    });
+});
